67b5d8a79270935a7885f2585e876e30cd4c7ac9
Kuka <lepkafill@mail.ru>
2023-03-14 19:39:16 +0300

    added gets for images and spaces

--- EventsApi/Features/Events/Data/TempSpaceData.cs
+++ EventsApi/Features/Events/Data/TempSpaceData.cs
@@ -1,4 +1,6 @@
-﻿namespace EventsApi.Features.Events.Data
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace EventsApi.Features.Events.Data
 {
     public class TempSpaceData
     {
@@ -8,6 +10,16 @@
             new UserSpace { Id=Guid.NewGuid(), Name="Space2" },
             new UserSpace { Id=Guid.NewGuid(), Name="Space3" },
         };
+
+        public static List<UserSpace> GetAll()
+        {
+            return spaces;
+        }
+
+        public static UserSpace? GetById(Guid id)
+        {
+            return spaces.FirstOrDefault(e => e.Id == id);
+        }
 
         public static bool CheckSpace(Guid id)
         {
