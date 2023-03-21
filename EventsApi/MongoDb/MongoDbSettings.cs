b0b920dde8075657fcbd8d4512cd595b0b4c3740
Kuka <lepkafill@mail.ru>
2023-03-21 05:08:20 +0300

    changed appsettings

--- EventsApi/MongoDb/MongoDbSettings.cs
+++ EventsApi/MongoDb/MongoDbSettings.cs
@@ -4,11 +4,17 @@
     {
         public string Host { get; set; }
         public int Port { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
         //public string ConnectionString => $"mongodb://{Host}:{Port}";
-        public string ConnectionString => $"mongodb://{Username}:{Password}@{Host}:{Port}";
-        
-
+        public string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+                    return $@"mongodb://{Host}:{Port}";
+                return $@"mongodb://{Username}:{Password}@{Host}:{Port}";
+            }
+        }
     }
 }
